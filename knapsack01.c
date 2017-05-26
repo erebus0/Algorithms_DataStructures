0548a146d703189484666e1ce7558dc21572a882
Ankit Tripathi <ankitmani.t@somaiya.edu>
2017-05-27 01:16:27 +0530

    Add 0-1 Knapsack Tracer

--- knapsack01.c
+++ knapsack01.c
@@ -12,6 +12,31 @@ void knapsack(int a[], int b[], int n, int W)
 	for(int i = 1; i <= n; i++)
 		for(int j = 1; j <= W; j++)
 			(b[i-1] > j) ? (dp[i][j] = dp[i-1][j]) : (dp[i][j] = MAX(dp[i-1][j], a[i-1] + dp[i-1][j-b[i-1]]));
+	int t = dp[n][W];
+	for(int i = n; i > 0; i--)
+	{
+		int l1 = i, l2 = W;
+		//printf("%d ",i);
+		if(dp[l1][l2] == t && l2 >= b[l1-1])
+		{
+			while((dp[l1-1][l2-b[l1-1]] != dp[l1][l2] - a[l1-1]))
+			{
+				l1--;i--;
+			}
+			while(l2)
+			{
+				while((dp[l1-1][l2-b[l1-1]] != dp[l1][l2] - a[l1-1]) && l2)
+				{
+					l1--;
+				}
+				if(l2)
+				{
+					printf("%d ",b[l1-1]);
+					l2 -= b[--l1];
+				}
+			}
+		}
+	}
 }
 
 void main()
