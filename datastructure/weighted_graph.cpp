0a05c75c1adadee7eb4b5cd930469e0fdccf2516
Ankit Tripathi <Ankitmani.t@somaiya.edu>
2017-07-31 02:30:58 -0700

    Update weighted_graph.cpp

--- datastructure/weighted_graph.cpp
+++ datastructure/weighted_graph.cpp
@@ -4,30 +4,31 @@ using namespace std;
 
 void make_edge(vector< vector< pair<int, int> > > &g, int n1, int n2, int w)
 {
-    g[n1].push_back(make_pair(n2, w));
-    g[n2].push_back(make_pair(n1, w));
+	g[n1].push_back(make_pair(n2, w));
+	g[n2].push_back(make_pair(n1, w));
 }
 
-void delete_edge(vector< vector< pair<int, int> > > &g, int n1, int n2)
+void delete_edge(vector< vector< pair<int, int> > > &g, int n1, int n2, int w)
 {
-    g[n1].erase(g[n1].find(g[n1].begin(), g[n1].end(), ))
+	g[n1].erase(find(g[n1].begin(), g[n1].end(), make_pair(n2, w)));
+	g[n2].erase(find(g[n2].begin(), g[n2].end(), make_pair(n1, w)));
 }
 
 vector< vector< pair<int, int> > > make_graph( int V)
 {
-    return vector< vector< pair<int, int> > >(V);
+	return vector< vector< pair<int, int> > >(V);
 }
 
 void print_adj_list(vector< vector< pair<int, int> > > g, int V)
 {
-    int i = 0;
-    for(auto l: g)
-    {
-        printf("%d ", i++);
-        for(auto n: l)
-            printf("%d-%d\t", n.first, n.second);
-        printf("\n");
-    }
+	int i = 0;
+	for(auto l: g)
+	{
+		printf("%d ", i++);
+		for(auto n: l)
+			printf("%d-%d\t", n.first, n.second);
+		printf("\n");
+	}
 }
 
 int main() {
@@ -37,7 +38,8 @@ int main() {
 	make_edge(graph, 1, 3, 4);
 	make_edge(graph, 0, 3, 2);
 	make_edge(graph, 4, 1, 8);
-	
+	print_adj_list(graph, 5);
+	delete_edge(graph, 0, 3, 2);
 	print_adj_list(graph, 5);
 	return 0;
 }
